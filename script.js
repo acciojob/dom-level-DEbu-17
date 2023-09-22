42332f18baaae78bd1ab0d3404e4c44fd7be4355
DEbu-17 <98536414+DEbu-17@users.noreply.github.com>
2023-09-22 12:39:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 //your JS code here. If required.
+let count = 1;
+let levelElement = document.getElementById("level");
+
+while(true){
+	if(levelElement.tagName === "HTML"){
+		break;
+	}
+	levelElement = levelElement.parentNode;
+	count++;
+}
+alert(`The level of the element is: ${count}`);
+
+
+
